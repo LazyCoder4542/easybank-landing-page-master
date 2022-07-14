bd7084317c01aa93a4535f025ef3a80802998af7
Adeola Fadare <adeolafadare8@gmail.com>
2022-07-14 11:40:13 +0100

    added media queries

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#open-menu, #close-menu').click(function() {
-        $('.menu ul').toggle('0');
+        $('.menu ul').toggle( "0" );
         $('#close-menu').toggle();
         $('#open-menu').toggle();
         //$('body').toggleClass('scroll-paused')
